d9139ed7c351853d49af12ab6c52a50285b43255
Zacharilius <zabensley@gmail.com>
2017-04-10 18:00:38 -0700

    IP: Meme dictionary created on backend. Exports Meme AppComponent to frontend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Meme {
+  title: string;
+  description: string;
+  url: string;
+}
+const MEMES: Meme[] = [
+  {title: 'test', description: 'descrition text', url: 'www.test.com'},
+  {title: 'test2', description: 'descrition text2', url: 'www.test.com2'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'app works!';
+  memes = MEMES;
 }
