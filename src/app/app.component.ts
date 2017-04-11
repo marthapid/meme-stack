0974732fb8b6461442aed6e7dbaa155480a1bfe5
Martha <marthapid@gmail.com>
2017-04-10 18:33:03 -0700

    Added voting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 export class Meme {
   title: string;
   description: string;
-  url: string;
+  source: string;
+  votes: number;
 }
 const MEMES: Meme[] = [
-  {title: 'test', description: 'descrition text', url: 'www.test.com'},
-  {title: 'test2', description: 'descrition text2', url: 'www.test.com2'}
+  {title: 'test', description: 'descrition text', source: 'www.test.com', votes: 0},
+  {title: 'test2', description: 'descrition text2', source: 'www.test.com2', votes: 0}
 ];
 
 @Component({
@@ -19,4 +20,12 @@ const MEMES: Meme[] = [
 export class AppComponent {
   title = 'app works!';
   memes = MEMES;
+
+
+  upvote(meme: Meme): void {
+    meme.votes++;
+  }
+  downvote(meme: Meme): void {
+    meme.votes--;
+  }
 }
